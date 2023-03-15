9dbc57f3d6d2533afd96173606a13e44472ac496
AlxSun <alexwcp@mail.ru>
2023-03-15 17:03:09 +0200

    Ввод данных от пользователя, не готово работаю

--- functions.py
+++ functions.py
@@ -8,54 +8,98 @@ session_vk = vk_api.VkApi(token=token)
 longpoll_vk = VkLongPoll(session_vk)
 #user_info_dct = {'bdate': '6.4', 'city': 61, 'sex': 2, 'first_name': 'Александр'}
 user_info_dct = {}
+check_info_lst = []
 
 def mssg_send(user_id, message):
     session_vk.method("messages.send", {
          "user_id" : user_id,
         "message" : message,
-        "random_id" : randrange(10**7)
-    })
+        "random_id" : randrange(10**7)})
 
 def mssg_send_foto(user_id, photo_id):
     URL = f"https://vk.com/{user_id}?z=photo{user_id}_{photo_id}%2Fphotos{user_id}"
     session_vk.method("messages.send", {
         "user_id" : user_id,
         "attachment" : URL,
-        "random_id": randrange (10 ** 7)
-    })
+        "random_id": randrange (10 ** 7)})
 
 def user_get_info(user_id):
     response = session_vk.method("users.get", {
         "user_id" : user_id,
         "v" : 5.131,
-        "fields" : "first_name, last_name, bdate, sex, city"
+        "fields" : "bdate, city, first_name"
         })
     if response:
-        user_info_dct["bdate"] = response[0]["bdate"]
-        user_info_dct["city"] = response[0]["city"]["id"]
-        user_info_dct["sex"] = response[0]["sex"]
-        user_info_dct["first_name"] = response[0]["first_name"]
+        for key, value in response[0].items():
+            if key == "bdate":
+                user_info_dct["bdate"] = response[0]["bdate"]
+            elif key == "sex":
+                user_info_dct["sex"] = response[0]["sex"]
+            elif key == "first_name":
+                user_info_dct["first_name"] = response[0]["first_name"]
+            elif key == "city":
+                user_info_dct["city"] = response[0]["city"]["id"]
     else:
-        mssg_send(user_id, "Ошибка")
+        mssg_send(user_id, "Ошибка сбора информиции о пользователе!")
         return False
     return user_info_dct
 
+#user_get_info(993117)
+
 def check_user_info(user_id, user_info_dct):
-    check_info = []
     for key in ['bdate', 'city', 'sex']:
         if not user_info_dct.get(key):
-            check_info.append(key)
+            check_info_lst.append(key)
     if user_info_dct.get('bdate'):
         if len(user_info_dct["bdate"].split(".")) !=3:
-            check_info.append("bdate")
-    if len(check_info) > 0:
-       mssg_send(user_id, f"Недостаточно информации, введите следующие данные: \n"
-                          f"{check_info}")
-"""           for event in longpoll_vk.listen ():
-               if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                   message = event.text.lower ()"""
-
-
-
+            check_info_lst.append("bdate")
+    if len(check_info_lst) > 0:
+       mssg_send(user_id, f"Недостаточно информации, введите команду (ввод)!")
+       #enter_information(user_id, check_info_lst)
+    return check_info_lst
+"""    print(f"Ваше имя: {user_info_dct['first_name']},\n"
+          f" пол: {user_info_dct['sex']},\n"
+          f"день рождения: {user_info_dct['bdate']},\n"
+          f" город в котором будет произведён поиск пары: {user_info_dct['city']}")"""
 
 #check_user_info(993117, user_info_dct)
+
+#check_info_lst = ['bdate', 'city', 'sex']
+
+def enter_information(user_id, check_info_lst):
+    print(check_info_lst)
+    for event in longpoll_vk.listen ():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            message = event.text.lower ()
+            if check_info_lst.count("bdate") > 0:
+                mssg_send(user_id, "Введите дату рождения: формат --> дд.мм.гггг")
+                if len(message.split(".")) == 3:
+                    user_info_dct["bdate"] = message
+                    check_info_lst.remove("bdate")
+                    print(check_info_lst)
+            elif check_info_lst.count("sex") > 0:
+                mssg_send(user_id, "Введите информацию о вашем поле:\n"
+                                   "женский --->  1\n"
+                                   "мужской ---> 2")
+                if message ==  2:
+                    user_info_dct["sex"] = message
+                    check_info_lst.remove ("sex")
+                    break
+
+
+"""def enter_information(user_id, check_info_lst):
+    for info_key in check_info_lst:
+        if info_key == "sex":
+            mssg_send(user_id, "В данных нехватает информации о Вашем поле,\n"
+                               "ответьте в сообщении:\n"
+                               "Женский пол -- > 'sex-1'\n"
+                               "Мужской пол -- > 'sex-2'")
+        elif info_key == "bdate":
+            mssg_send(user_id, "Информация о Вашем дне рождении отсутствует или она не полная.\n"
+                               "Прошу написать в сообщении день рождения,\n"
+                               "в следующем формате: --> {'bdate' : дд.мм.гггг}")"""
+
+
+
+
+
--- main.py
+++ main.py
@@ -7,34 +7,36 @@ from functions import *
 from vk_api.longpoll import VkLongPoll, VkEventType
 from Token import token
 
-#user_id ="993117"
-#user_get_info(66206783)
-
 session_vk = vk_api.VkApi(token=token) # авторизация, для применения метода пишем method
 get_vk = session_vk.get_api() # позволяет обращаться к методам через точку
 longpoll_vk = VkLongPoll(session_vk) #
+
 
 
 for event in longpoll_vk.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         message = event.text.lower()
         user_id = event.user_id
-        user_get_info(user_id)
         if message == "привет":
-            mssg_send(user_id, f"Я тут {user_info_dct['first_name']}, привет! \n"
-                               f"Если Вы {user_info_dct['first_name']} хотите узнать о всех доступных функциях, \n"
-                               f"напечатайте в сообщении слово: -- > меню")
+            mssg_send(user_id, "Я тут, привет! \n"
+                               "Если Вы  хотите узнать о всех доступных функциях, \n"
+                               "напечатайте в сообщении слово: -- > меню")
         elif message == "меню":
-            mssg_send(user_id, f"1 -- Сбор информации о Вас и уточнение данных для поиска пары!\n"
-                               f"2 -- Возрастной диапазон для поиска пары.\n"
-                               f"3 -- ")
+            mssg_send(user_id, "1 -- Сбор информации о Вас и уточнение данных для поиска пары!\n"
+                               "2 -- Возрастной диапазон для поиска пары.\n"
+                               "3 -- Мои данные")
         elif message == "1":
+            user_get_info (user_id)
             check_user_info(user_id, user_info_dct)
-            #mssg_send (user_id, f"Вот твои данные {user_info_dct}")
+        elif message == "ввод":
+            pass
+        elif message == "3":
+            user_get_info(user_id)
+            mssg_send(user_id, f"Вот инфа о тебе: {user_info_dct}")
         elif message == "фото":
-            mssg_send(user_id, f"Вот тебе {user_info_dct['first_name']}, фото!")
+            mssg_send(user_id, "Вот   фото!")
             mssg_send_foto(user_id, "457239119")
         else:
-            mssg_send(user_id, f"Я пока знаю очеь мало слов!!!, {user_info_dct['first_name']} научи меня.")
+            mssg_send(user_id, f"Я пока знаю очень мало слов!!!")
 
 
